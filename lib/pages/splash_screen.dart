086c3862287b8fc830bf559d1c5bf49e0c06ba2c
Layhome <78995627+layhome12@users.noreply.github.com>
2022-05-29 20:59:02 +0700

    splash_screen.dart pubspec.yaml

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -21,16 +21,16 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(milliseconds: 2000), (){
       setState(() {
         Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => const LoginPage()), (route) => false);
+            MaterialPageRoute(builder: (context) => const LoginPage()), (route) => false);
       });
     });
 
     Timer(
-      const Duration(milliseconds: 10),(){
-        setState(() {
-          _isVisible = true; // Now it is showing fade effect and navigating to Login page
-        });
-      }
+        Duration(milliseconds: 10),(){
+      setState(() {
+        _isVisible = true; // Now it is showing fade effect and navigating to Login page
+      });
+    }
     );
 
   }
@@ -39,38 +39,28 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
 
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
           colors: [Theme.of(context).accentColor, Theme.of(context).primaryColor],
           begin: const FractionalOffset(0, 0),
           end: const FractionalOffset(1.0, 0.0),
-          stops: const [0.0, 1.0],
+          stops: [0.0, 1.0],
           tileMode: TileMode.clamp,
         ),
       ),
       child: AnimatedOpacity(
         opacity: _isVisible ? 1.0 : 0,
-        duration: const Duration(milliseconds: 1200),
+        duration: Duration(milliseconds: 1200),
         child: Center(
           child: Container(
-            height: 140.0,
-            width: 140.0,
-            child: const Center(
-              child: ClipOval(
-                child: Icon(Icons.android_outlined, size: 128,), //put your logo here
+            height: 1500,
+            width: 1000,
+            child: Center(
+              child: Image.asset(
+                "assets/images/kantongku-removebg-preview.png",
+                width: 1500,
+                height: 1000,
               ),
-            ),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  blurRadius: 2.0,
-                  offset: const Offset(5.0, 3.0),
-                  spreadRadius: 2.0,
-                )
-              ]
             ),
           ),
         ),
